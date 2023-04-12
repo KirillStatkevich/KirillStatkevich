e902b014b08138d88dd94d4b67c691db66551819
KirillStatkevich <128984145+KirillStatkevich@users.noreply.github.com>
2023-04-12 13:46:22 +0300

    Add files via upload

---
+++ igra_ygadai_cifry.py
@@ -0,0 +1,7 @@
+import random
+my_number=random.randint(0, 10)
+your_number=int(input('write choosen nubmer\n'))
+if my_number==your_number:
+    print('your lucky!')
+else:
+    not print('your a losssseerrrr, my_number is:') and print(my_number)
---
+++ strings.py
@@ -0,0 +1,3 @@
+x="ylibok tebe ded makar"
+print(len(x))
+print(x[::-1])
