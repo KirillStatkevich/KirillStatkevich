905132cf9ed6cfecbbf5eafedbe6e74183b3779b
KirillStatkevich <Smile-kirill99@yandex.ru>
2023-04-12 15:54:50 +0300

    test

--- test/igra_ygadai_cifry.py
+++
@@ -1,7 +0,0 @@
-import random
-my_number=random.randint(0, 10)
-your_number=int(input('write choosen nubmer\n'))
-if my_number==your_number:
-    print('your lucky!')
-else:
-    not print('your a losssseerrrr, my_number is:') and print(my_number)
--- test/poloshitelno_ili_otritsatelno.py
+++
@@ -1,8 +0,0 @@
-x=int (input('enter your number'))
-if x==0:
-    print ('your number is zero')
-elif x>0:
-    print ('your number is more then zero')
-elif x<0:
-    print ('your number is under then zero')
-print ('program is end')
