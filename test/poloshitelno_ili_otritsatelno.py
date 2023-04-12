b2e5d5585e824a2dc95a874703015abb2d2ea18e
KirillStatkevich <Smile-kirill99@yandex.ru>
2023-04-12 15:42:48 +0300

    Create poloshitelno_ili_otritsatelno.py

---
+++ test/poloshitelno_ili_otritsatelno.py
@@ -0,0 +1,8 @@
+x=int (input('enter your number'))
+if x==0:
+    print ('your number is zero')
+elif x>0:
+    print ('your number is more then zero')
+elif x<0:
+    print ('your number is under then zero')
+print ('program is end')
