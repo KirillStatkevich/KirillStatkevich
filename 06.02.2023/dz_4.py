3028557b4e0c992d81d987979a78b623ff8110c8
KirillStatkevich <Smile-kirill99@yandex.ru>
2023-04-23 21:18:49 +0300

    Create dz_4.py

---
+++ 06.02.2023/dz_4.py
@@ -0,0 +1,29 @@
+l= input("введите список чисел\n")
+m,v1,q,v=0,0,0,0
+for i in l:
+    if i[0]<=l[m]:
+        m=m+1
+        i=int(i)
+        v=int(v)
+        if v<i:
+            v=i
+            v1=m-1
+        continue
+x=list(l)
+z=[]
+for i in x:                 #c помощью этой функции удаляю дубликаты не нужного числа
+    if i not in z:          #если в i не присутствует в z, то мы его добавляем
+        z.append(i)
+x=z
+  
+del x[v1]
+v,m,i=0,0,0
+for i in x:
+    if i[0]<=x[m]:
+        m=m+1
+        i=int(i)
+        v=int(v)
+        if v<i:
+            v=i
+        continue
+print("Предпоследнее число по величине это: ",v)
